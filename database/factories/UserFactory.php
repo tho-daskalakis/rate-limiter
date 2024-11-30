a25cf95a1969a9889a1c54bfbb0545ff73fc412d
thomas-d <tho.daskalakis@gmail.com>
2024-12-01 00:52:10 +0200

    fix user factory producing same passwords

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make(fake()->password()),
             'remember_token' => Str::random(10),
             'timezone' => Arr::random(['CET', 'CST', 'GMT+1']),
         ];
