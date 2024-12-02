ec43556b82a641cbcad20e6d6c004f835ad476ad
thomas-d <tho.daskalakis@gmail.com>
2024-12-03 00:05:32 +0200

    add user routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller('App\Http\Controllers\UserController')->group(function () {
+   // ...other user routes
+
+    Route::patch('/users/{user}', 'update');
+});
