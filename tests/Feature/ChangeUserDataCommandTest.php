a948d0847a123cee757e4864446bceff4f673ea1
thomas-d <tho.daskalakis@gmail.com>
2025-03-16 16:31:47 +0200

    test that user's name is changed after command

--- tests/Feature/ChangeUserDataCommandTest.php
+++ tests/Feature/ChangeUserDataCommandTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class ChangeUserDataCommandTest extends TestCase
@@ -12,6 +13,19 @@ class ChangeUserDataCommandTest extends TestCase
 
     public function testUserCount(): void {
         $this->assertDatabaseCount('users', 20);
+    }
+
+    public function testUserNameChanged(): void {
+        $user1 = DB::table('users')->where('id', 1)->first();
+
+        // Run the command
+        $this->artisan('app:change-user-data');
+
+        $changedUser1 = DB::table('users')->where('id', 1)->first();
+        //         print "$user1->name, $user1->email\n";`
+        //         print "$changedUser1->name, $user1->email\n";
+
+        $this->assertNotEquals($changedUser1->name, $user1->name);
     }
 
 }
