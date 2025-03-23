346933217a73dedbd3fca5d54eb0c43b350c7f54
thomas-d <tho.daskalakis@gmail.com>
2025-03-23 16:39:33 +0200

    add model attributes that correspond to table cols

--- app/Models/PendingUpdate.php
+++ app/Models/PendingUpdate.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class PendingUpdate extends Model
 {
     protected $attributes = [
+        'user_id' => 'int',
+        'data_hash' => 'string',
         'status' => 'pending',
         'tries' => 0
     ];
