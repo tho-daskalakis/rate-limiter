cf9e6a4e84c355e783ec986f32ac6b96ed0f38f4
thomas-d <tho.daskalakis@gmail.com>
2025-03-28 23:27:50 +0200

    add name to root route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::controller('App\Http\Controllers\UserController')->group(function () {
    // ...other user routes
